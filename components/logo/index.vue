3bcddc49e452a35be94ba32aa4bd50ab2c7a6dd0
gustavokozlowski <guskozlowskidev@gmail.com>
2024-05-12 01:59:14 -0300

    feat: adiciona componente logo

---
+++ components/logo/index.vue
@@ -0,0 +1,19 @@
+<template>
+    <div :class="$style.logo_container">
+        <img src="../../assets/images/bunny.svg" alt=" bunny rabit">
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style module>
+.logo_container {
+    height: fit-content;
+    width: fit-content;
+}
+.logo_container img {
+    height: 7vh;
+}
+</style>
