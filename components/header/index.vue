208b4638029896036d386743e036d57bcbb03b0c
gustavokozlowski <guskozlowskidev@gmail.com>
2024-05-12 02:04:14 -0300

    feat: adiciona component header

--- components/header/index.vue
+++ components/header/index.vue
@@ -1,8 +1,11 @@
 <template>
   <header :class="$style.header_container">
-    <nav :class="$style.header_content">
+    <span>
+      <logo />
       <h1>ShorlowWorld's</h1>
-      <ul :class="$style.nav_list" >
+    </span>
+    <nav :class="$style.header_content">
+      <ul :class="$style.nav_list">
         <li>home</li>
         <li>projetos</li>
         <li>experiências</li>
@@ -17,35 +20,52 @@ import '../../assets/css/globals.css'
 
 </script>
 <style module >
-.header_container{
+.header_container {
   display: flex;
-    background-color: var(--main-bg-color);
-  
-
-}
-
-.header_content{
-  display: flex;
-  justify-content: space-around;
-  background-color: bisque;
-  width: 100%;
-}
-
-.nav_list{
-  list-style: none;
-  justify-content: space-around;
+  background: var(--main-bg-color);
+  height: 10vh;
   align-items: center;
-  gap: 2px;
+  border-bottom: 1px solid var(--primary-color);
+  /* box-shadow: 0.2px 2px 0px 0px #ddd; */
+}
+
+.header_container span {
   display: flex;
-  background-color: blueviolet;
-  width: 30%;
+  /* background-color: rgb(46, 32, 15); */
+  align-items: center;
+  height: 100%;
+  width: 40vw;
+  justify-content: center;
 
-  > li {
+  h1 {
+    font-size: 1.4rem;
 
-    &:hover{
-      cursor: pointer;
-    }
   }
 }
 
+.header_content {
+  display: flex;
+  justify-content: right;
+  /* background-color: rgb(228, 216, 202); */
+  width: 60vw;
+  height: 100%;
+}
+
+.nav_list {
+  list-style: none;
+  justify-content: center;
+  align-items: center;
+  display: flex;
+  /* background-color: blueviolet; */
+  width: 30vw;
+  gap: 1rem;
+  >li {
+    transition: color 300ms linear;
+    font-size: 1rem;
+    &:hover {
+      cursor: pointer;
+      color: var(--primary-color)
+    }
+  }
+}
 </style>
